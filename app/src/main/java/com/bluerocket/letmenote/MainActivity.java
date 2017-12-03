c301cf6324453090fc7db8a9b8c08f60f07684fa
nehaluddin.haider.mohammed <nehaluddin.haider.mohammed@intl.verizon.com>
2017-12-03 18:14:33 +0530

    Initial commit

---
+++ app/src/main/java/com/bluerocket/letmenote/MainActivity.java
@@ -0,0 +1,193 @@
+package com.bluerocket.letmenote;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gordonwong.materialsheetfab.MaterialSheetFab;
+import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout;
+import nl.psdcompany.duonavigationdrawer.views.DuoMenuView;
+import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DuoMenuView.OnMenuClickListener {
+    private MenuAdapter mMenuAdapter;
+    private ViewHolder mViewHolder;
+    public Fab fab;
+
+    int lastSelectedPosition = 0;
+    private MaterialSheetFab materialSheetFab;
+    private int statusBarColor;
+
+    private ArrayList<String> mTitles = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTitles = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.menuOptions)));
+
+        // Initialize the views
+        mViewHolder = new ViewHolder();
+
+        // Handle toolbar actions
+        handleToolbar();
+
+        // Handle menu actions
+        handleMenu();
+
+        // Handle drawer actions
+        handleDrawer();
+
+        // Show main fragment in container
+        goToFragment(new MainFragment(), false);
+        mMenuAdapter.setViewSelected(0, true);
+
+
+        setupFab();
+        materialSheetFab.showFab();
+        setTitle(mTitles.get(0));
+    }
+
+    private void handleToolbar() {
+        setSupportActionBar(mViewHolder.mToolbar);
+    }
+
+    private void handleDrawer() {
+        DuoDrawerToggle duoDrawerToggle = new DuoDrawerToggle(this,
+                mViewHolder.mDuoDrawerLayout,
+                mViewHolder.mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        mViewHolder.mDuoDrawerLayout.setDrawerListener(duoDrawerToggle);
+        duoDrawerToggle.syncState();
+
+    }
+
+    private void handleMenu() {
+        mMenuAdapter = new MenuAdapter(mTitles);
+
+        mViewHolder.mDuoMenuView.setOnMenuClickListener(this);
+        mViewHolder.mDuoMenuView.setAdapter(mMenuAdapter);
+    }
+
+    @Override
+    public void onFooterClicked() {
+        Toast.makeText(this, "onFooterClicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onHeaderClicked() {
+        Toast.makeText(this, "onHeaderClicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void goToFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.add(R.id.container, fragment).commit();
+    }
+
+    @Override
+    public void onOptionClicked(int position, Object objectClicked) {
+        // Set the toolbar title
+        setTitle(mTitles.get(position));
+
+        // Set the right options selected
+        mMenuAdapter.setViewSelected(position, true);
+
+        // Navigate to the right fragment
+        switch (position) {
+            default:
+                goToFragment(new MainFragment(), false);
+                break;
+        }
+
+        // Close the drawer
+        mViewHolder.mDuoDrawerLayout.closeDrawer();
+    }
+
+    private class ViewHolder {
+        private DuoDrawerLayout mDuoDrawerLayout;
+        private DuoMenuView mDuoMenuView;
+        private Toolbar mToolbar;
+
+        ViewHolder() {
+            mDuoDrawerLayout = (DuoDrawerLayout) findViewById(R.id.drawer);
+            mDuoMenuView = (DuoMenuView) mDuoDrawerLayout.getMenuView();
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+    }
+
+    public void setupFab() {
+
+        fab = (Fab) findViewById(R.id.fab);
+        View sheetView = findViewById(R.id.fab_sheet);
+        View overlay = findViewById(R.id.overlay);
+
+
+        int sheetColor = getResources().getColor(R.color.background_card);
+        int fabColor = getResources().getColor(R.color.theme_accent);
+
+        // Create material sheet FAB
+        materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay, sheetColor, fabColor);
+
+        findViewById(R.id.fab_sheet_item_recording).setOnClickListener(this);
+        findViewById(R.id.fab_sheet_item_reminder).setOnClickListener(this);
+        findViewById(R.id.fab_sheet_item_photo).setOnClickListener(this);
+        findViewById(R.id.fab_sheet_item_note).setOnClickListener(this);
+
+        // Set material sheet event listener
+        materialSheetFab.setEventListener(new MaterialSheetFabEventListener() {
+            @Override
+            public void onShowSheet() {
+                // Save current status bar color
+                statusBarColor = getStatusBarColor();
+                // Set darker status bar color to match the dim overlay
+                setStatusBarColor(getResources().getColor(R.color.theme_primary_dark2));
+            }
+
+            @Override
+            public void onHideSheet() {
+                // Restore status bar color
+                setStatusBarColor(statusBarColor);
+            }
+        });
+
+        // Set material sheet item click listeners
+
+    }
+
+    private int getStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return getWindow().getStatusBarColor();
+        }
+        return 0;
+    }
+
+    private void setStatusBarColor(int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(color);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+    //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddNoteFragment()).commitAllowingStateLoss();
+        materialSheetFab.hideSheet();
+    }
+}
